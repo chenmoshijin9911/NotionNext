9b3d87cd3b11cd624e15ee6e1629a3ad7f8b1f0e
chenmoshijin9911 <166527338+chenmoshijin9911@users.noreply.github.com>
2026-03-20 20:59:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享技能',
+  HEO_HERO_TITLE_2: '与前沿科技',
+  HEO_HERO_TITLE_3: '小白的个人博客',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '我的YouTube频道',
+  HEO_HERO_TITLE_LINK: 'https://www.youtube.com/@%E5%B0%8F%E7%99%BD%E7%BF%BB%E5%A2%99%E6%94%BB%E7%95%A5',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -51,76 +51,76 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: 'https://www.youtube.com/@%E5%B0%8F%E7%99%BD%E7%BF%BB%E5%A2%99%E6%94%BB%E7%95%A5',
+  HEO_INFO_CARD_ICON2: 'fab fa-telegram',
+  HEO_INFO_CARD_URL3: 'https://t.me/xiaobai0044',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
+      title_1: 'YouTube',
+      img_1: 'https://cdn.simpleicons.org/youtube/FF0000',
+      color_1: '#FF0000',
+      title_2: 'X',
+      img_2: 'https://cdn.simpleicons.org/x/000000',
+      color_2: '#000000'
     },
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
+      title_1: 'Telegram',
+      img_1: 'https://cdn.simpleicons.org/telegram/26A5E4',
+      color_1: '#26A5E4',
+      title_2: 'Discord',
+      img_2: 'https://cdn.simpleicons.org/discord/5865F2',
+      color_2: '#5865F2'
     },
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-      color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
-      color_2: '#ffffff'
+      title_1: 'Instagram',
+      img_1: 'https://cdn.simpleicons.org/instagram/E4405F',
+      color_1: '#E4405F',
+      title_2: 'WhatsApp',
+      img_2: 'https://cdn.simpleicons.org/whatsapp/25D366',
+      color_2: '#25D366'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
+      title_1: 'Reddit',
+      img_1: 'https://cdn.simpleicons.org/reddit/FF4500',
+      color_1: '#FF4500',
+      title_2: 'TikTok',
+      img_2: 'https://cdn.simpleicons.org/tiktok/000000',
+      color_2: '#000000'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      title_1: 'Facebook',
+      img_1: 'https://cdn.simpleicons.org/facebook/1877F2',
+      color_1: '#1877F2',
+      title_2: 'Twitch',
+      img_2: 'https://cdn.simpleicons.org/twitch/9146FF',
+      color_2: '#9146FF'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      title_1: 'Google',
+      img_1: 'https://cdn.simpleicons.org/google/4285F4',
+      color_1: '#4285F4',
+      title_2: 'Netflix',
+      img_2: 'https://cdn.simpleicons.org/netflix/E50914',
+      color_2: '#E50914'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      title_1: 'Spotify',
+      img_1: 'https://cdn.simpleicons.org/spotify/1DB954',
+      color_1: '#1DB954',
+      title_2: 'Steam',
+      img_2: 'https://cdn.simpleicons.org/steam/1B2838',
+      color_2: '#1B2838'
     }
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我们的电报群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/xiaobai0044',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
